657af63c91eda245c34b28b81a47ff18b5b38e6e
Troubledesigner <821878167@qq.com>
2017-03-30 23:03:58 +0800

    new

--- Page20170326/work2.c
+++ Page20170326/work2.c
@@ -4,57 +4,57 @@ void main() {
     int t;
     float n1, n2;
     char c;
-    int temp;
     while (scanf("%d %f %f %c", &t, &n1, &n2, &c) != EOF) {
+        getchar();
         if(t == 1) {
             switch (c) {
                 case '+':
-                    printf("After switch processing, the result is : %.2f\n", n1 + n2);
+                    printf("After switch processing,the result is : %.2f\n", n1 + n2);
                     break;
                 case '-':
-                    printf("After switch processing, the result is : %.2f\n", n1 - n2);
+                    printf("After switch processing,the result is : %.2f\n", n1 - n2);
                     break;
                 case '*':
-                    printf("After switch processing, the result is : %.2f\n", n1 * n2);
+                    printf("After switch processing,the result is : %.2f\n", n1 * n2);
                     break;
                 case '/':
-                    printf("After switch processing, the result is : %.2f\n", n1 / n2);
+                    printf("After switch processing,the result is : %.2f\n", n1 / n2);
                     break;
                 default:
                     break;
             }
         } else if (t == 0) {
             if (c == '+') {
-                printf("After if-else processing, the result is : %.2f\n", n1 + n2);
+                printf("After if-else processing,the result is : %.2f\n", n1 + n2);
             } else if (c == '-') {
-                printf("After if-else processing, the result is : %.2f\n", n1 - n2);
+                printf("After if-else processing,the result is : %.2f\n", n1 - n2);
             } else if (c == '*') {
-                printf("After if-else processing, the result is : %.2f\n", n1 * n2);
+                printf("After if-else processing,the result is : %.2f\n", n1 * n2);
             } else if (c == '/') {
-                printf("After if-else processing, the result is : %.2f\n", n1 / n2);
+                printf("After if-else processing,the result is : %.2f\n", n1 / n2);
             }
         } else if (t == 2) {
             if (c == '+') {
-                printf("After if-else processing, the result is : %.2f\n", n1 + n2);
+                printf("After if-else processing,the result is : %.2f\n", n1 + n2);
             } else if (c == '-') {
-                printf("After if-else processing, the result is : %.2f\n", n1 - n2);
+                printf("After if-else processing,the result is : %.2f\n", n1 - n2);
             } else if (c == '*') {
-                printf("After if-else processing, the result is : %.2f\n", n1 * n2);
+                printf("After if-else processing,the result is : %.2f\n", n1 * n2);
             } else if (c == '/') {
-                printf("After if-else processing, the result is : %.2f\n", n1 / n2);
+                printf("After if-else processing,the result is : %.2f\n", n1 / n2);
             }
             switch (c) {
                 case '+':
-                    printf("After switch processing, the result is : %.2f\n", n1 + n2);
+                    printf("After switch processing,the result is : %.2f\n", n1 + n2);
                     break;
                 case '-':
-                    printf("After switch processing, the result is : %.2f\n", n1 - n2);
+                    printf("After switch processing,the result is : %.2f\n", n1 - n2);
                     break;
                 case '*':
-                    printf("After switch processing, the result is : %.2f\n", n1 * n2);
+                    printf("After switch processing,the result is : %.2f\n", n1 * n2);
                     break;
                 case '/':
-                    printf("After switch processing, the result is : %.2f\n", n1 / n2);
+                    printf("After switch processing,the result is : %.2f\n", n1 / n2);
                     break;
                 default:
                     break;
