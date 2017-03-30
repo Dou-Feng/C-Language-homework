d3d22f4c7a127988c91ddb6d42fa8dfe7cc151ba
Troubledesigner <821878167@qq.com>
2017-03-30 23:53:40 +0800

    must use double and initlize

--- Page20170326/work2.c
+++ Page20170326/work2.c
@@ -1,61 +1,61 @@
 #include "stdio.h"
 
 int main() {
-    int t;
-    float n1, n2;
-    char c;
-    while (scanf("%d %f %f %c", &t, &n1, &n2, &c) == 4) {
+    int t = 0;
+    double n1 = 0, n2 = 0;
+    char c = '1';
+    while (scanf("%d  %lf  %lf %c", &t, &n1, &n2, &c) == 4) {
         if(t == 1) {
             switch (c) {
-                case '+':
-                    printf("After switch processing,the result is : %.2f\n", n1 + n2);
+                case '+' : 
+                    printf("After switch processing,the result is : %.2lf\n", n1 + n2);
                     break;
-                case '-':
-                    printf("After switch processing,the result is : %.2f\n", n1 - n2);
+                case '-' : 
+                    printf("After switch processing,the result is : %.2lf\n", n1 - n2);
                     break;
-                case '*':
-                    printf("After switch processing,the result is : %.2f\n", n1 * n2);
+                case '*' : 
+                    printf("After switch processing,the result is : %.2lf\n", n1 * n2);
                     break;
-                case '/':
-                    printf("After switch processing,the result is : %.2f\n", n1 / n2);
+                case '/' : 
+                    printf("After switch processing,the result is : %.2lf\n", n1 / n2);
                     break;
-                default:
+                default : 
                     break;
             }
         } else if (t == 0) {
             if (c == '+') {
-                printf("After if-else processing,the result is : %.2f\n", n1 + n2);
+                printf("After if-else processing,the result is : %.2lf\n", n1 + n2);
             } else if (c == '-') {
-                printf("After if-else processing,the result is : %.2f\n", n1 - n2);
+                printf("After if-else processing,the result is : %.2lf\n", n1 - n2);
             } else if (c == '*') {
-                printf("After if-else processing,the result is : %.2f\n", n1 * n2);
+                printf("After if-else processing,the result is : %.2lf\n", n1 * n2);
             } else if (c == '/') {
-                printf("After if-else processing,the result is : %.2f\n", n1 / n2);
+                printf("After if-else processing,the result is : %.2lf\n", n1 / n2);
             }
         } else if (t == 2) {
             if (c == '+') {
-                printf("After if-else processing,the result is : %.2f\n", n1 + n2);
+                printf("After if-else processing,the result is : %.2lf\n", n1 + n2);
             } else if (c == '-') {
-                printf("After if-else processing,the result is : %.2f\n", n1 - n2);
+                printf("After if-else processing,the result is : %.2lf\n", n1 - n2);
             } else if (c == '*') {
-                printf("After if-else processing,the result is : %.2f\n", n1 * n2);
+                printf("After if-else processing,the result is : %.2lf\n", n1 * n2);
             } else if (c == '/') {
-                printf("After if-else processing,the result is : %.2f\n", n1 / n2);
+                printf("After if-else processing,the result is : %.2lf\n", n1 / n2);
             }
             switch (c) {
-                case '+':
-                    printf("After switch processing,the result is : %.2f\n", n1 + n2);
+                case '+' : 
+                    printf("After switch processing,the result is : %.2lf\n", n1 + n2);
                     break;
-                case '-':
-                    printf("After switch processing,the result is : %.2f\n", n1 - n2);
+                case '-' : 
+                    printf("After switch processing,the result is : %.2lf\n", n1 - n2);
                     break;
-                case '*':
-                    printf("After switch processing,the result is : %.2f\n", n1 * n2);
+                case '*' : 
+                    printf("After switch processing,the result is : %.2lf\n", n1 * n2);
                     break;
-                case '/':
-                    printf("After switch processing,the result is : %.2f\n", n1 / n2);
+                case '/' : 
+                    printf("After switch processing,the result is : %.2lf\n", n1 / n2);
                     break;
-                default:
+                default : 
                     break;
             }
         }
